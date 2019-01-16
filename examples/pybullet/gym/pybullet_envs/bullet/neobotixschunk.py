5ca8f14c59a309564cc7f517dc8795946795135b
leiiiiii <wangxiaolei0430@gmail.com>
2019-01-16 15:01:37 +0100

    modify

--- examples/pybullet/gym/pybullet_envs/bullet/neobotixschunk.py
+++ examples/pybullet/gym/pybullet_envs/bullet/neobotixschunk.py
@@ -1,4 +1,4 @@
-import os,  inspect
+#import os,  inspect
 #
 # currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 # parentdir = os.path.dirname(os.path.dirname(currentdir))
@@ -15,7 +15,7 @@ import math
 
 class NeobotixSchunk:
 
-    def __init__(self, urdfRootPath=pybullet_data.getDataPath(), timeStep=0.01):
+    def __init__(self, urdfRootPath='/home/lei/Documents/Projektpraktikum/Pybullet/bullet3/examples/pybullet/gym/pybullet_data/', timeStep=0.01):
         self.urdfRootPath = urdfRootPath
         self.timeStep = timeStep
         self.maxVelocity = .35
@@ -29,9 +29,9 @@ class NeobotixSchunk:
         self.jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 
     def reset(self):
-        objects = p.loadURDF(os.path.join(self.urdfRootPath,"neobotixschunk/NeobotixSchunk.urdf"))
-        print 'ob', objects
-        self.neobotixschunkUid = objects
+        #os.path.join: path combination and return
+        #load urdf returns a uniqueId,a noninteger value
+        self.neobotixschunkUid = p.loadURDF('/home/lei/Documents/Projektpraktikum/Pybullet/bullet3/examples/pybullet/gym/pybullet_data/neobotixschunk/NeobotixSchunk.urdf')
 
         for i in range(p.getNumJoints(self.neobotixschunkUid)):
             print(p.getJointInfo(self.neobotixschunkUid, i))
@@ -46,12 +46,12 @@ class NeobotixSchunk:
             p.resetJointState(self.neobotixschunkUid, wheel,initial_wheelVel[wheel])
             p.setJointMotorControl2(self.neobotixschunkUid, wheel, p.VELOCITY_CONTROL, targetVelocity=initial_wheelVel[wheel],force=self.maxForce)
 
-        for joint in (self.joints):
+        for joint in self.joints:
             p.resetJointState(self.neobotixschunkUid,joint,initial_jointstate[joint-6])
             p.setJointMotorControl2(self.neobotixschunkUid, joint, p.POSITION_CONTROL,targetPosition=initial_jointstate[joint-6], force=self.maxForce)
 
     def getActionDimension(self):
-        return 9
+        return 5
 
     def getObservationDimension(self):
         return len(self.getObservation())
@@ -59,6 +59,7 @@ class NeobotixSchunk:
     def getObservation(self):
         observation = []
         state = p.getLinkState(self.neobotixschunkUid, self.neobotixschunkEndEffectorIndex)
+        print(state)
         pos = state[0]
         orn = state[1]
         euler = p.getEulerFromQuaternion(orn)
@@ -68,12 +69,23 @@ class NeobotixSchunk:
 
         return observation
 
-    def applyAction(self, Commands):
-        targetVelocity = Commands[0]
-        targetangle = Commands[1]
+    def applyAction(self, action):
+        targetVelocityL = action[0]
+        targetVelocityR = action[1]
+        targetvelocity=[targetVelocityL,targetVelocityR]
+        print('targetVelocity=',targetvelocity)
+        targetangle_1=action[2]
+        targetangle_2 = action[3]
+        targetangle_3 = action[4]
+        targetangle_4 = action[5]
+        targetangle_5 = action[6]
+        targetangle_6 = action[7]
+        targetangle_7 = action[8]
+        targetangle = [targetangle_1,targetangle_2,targetangle_3,targetangle_4,targetangle_5,targetangle_6,targetangle_7]
+        print('targetAngle=', targetangle)
         for motor in self.wheels:
             p.setJointMotorControl2(self.neobotixschunkUid, motor,p.VELOCITY_CONTROL,
-                                          targetVelocity=targetVelocity, force=self.maxForce)
+                                          targetVelocity=targetvelocity[motor], force=self.maxForce)
         for motor_2 in self.joints:
             p.setJointMotorControl2(self.neobotixschunkUid, motor_2, p.POSITION_CONTROL,
-                                          targetPosition=targetangle)
+                                          targetPosition=targetangle[motor_2-6],force=self.maxForce)
