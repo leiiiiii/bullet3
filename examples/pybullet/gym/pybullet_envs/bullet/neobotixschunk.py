18741080811fe46aac3efebfa7e6e38917444da4
leiiiiii <wangxiaolei0430@gmail.com>
2019-02-21 09:58:12 +0100

    add applyaction2

--- examples/pybullet/gym/pybullet_envs/bullet/neobotixschunk.py
+++ examples/pybullet/gym/pybullet_envs/bullet/neobotixschunk.py
@@ -9,7 +9,7 @@ sys.path.append('/home/lei/Documents/Projektpraktikum/Pybullet/bullet3/examples/
 
 import pybullet as p
 import numpy as np
-import pybullet_data
+# import pybullet_data
 import math
 
 
@@ -24,19 +24,26 @@ class NeobotixSchunk:
         self.useSimulation = 1
         self.useNullSpace = 0
         self.useOrientation = 1
+        self.useInverseKinematics = 1
         self.neobotixschunkEndEffectorIndex = 13
         self.wheels = [0, 1]
         self.joints = [6, 7, 8, 9, 10, 11, 12]
+        self.j1_limit = 3.1415  # limits for arm link 1, 3, 5
+        self.j4_limit = 2.1118  #121 / 180 * np.pi   limits for arm link 4
+        self.j6_limit = 2.0071  #115 / 180 * np.pi  # limits for arm link 2, 6
+        self.j7_limit = 2.9671  #170 / 180 * np.pi  # limits for arm link 7
         self.reset()
 
         # lower limits for null space
-        self.ll = [-3.1215926, -2.12, -3.1215926, -2.16, -3.1215926, -2.07, -2.94]
+        self.ll = [-self.j1_limit,-self.j1_limit,-self.j1_limit, -self.j6_limit, -self.j1_limit, -self.j4_limit, -self.j1_limit, -self.j6_limit, -self.j7_limit]
         # upper limits for null space
-        self.ul = [3.1215926, 2.12, 3.1215926, 2.16, 3.1215926, 2.07, 2.94]
+        self.ul = [self.j1_limit,self.j1_limit,self.j1_limit, self.j6_limit, self.j1_limit, self.j4_limit, self.j1_limit, self.j6_limit, self.j7_limit]
         # joint ranges for null space
-        self.jr = [6.24, 4.24, 6.24, 4.32, 6.24, 4.14, 5.88]
+        self.jr = [6.283,6.283,6.283, 4.0142, 6.283, 4.2236, 6.283, 4.0142, 5.9344]
         # joint damping coefficents
-        self.jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+        self.jd = [0.1,0.1,0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+        # restposes for null space
+        self.rp = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     def reset(self):
         #os.path.join: path combination and return
@@ -51,6 +58,7 @@ class NeobotixSchunk:
         initial_jointstate=[0, 0, 0, 0, 0, 0, 0]
         self.basevelocity = 0
         self.baseangulervelocity = 0
+        self.endEffectorPos = [0.19, 0.0, 1.147]
 
 
         for wheel in self.wheels:
@@ -97,31 +105,61 @@ class NeobotixSchunk:
             base_ang = base_ang - delta_ba
         return base_ang
 
+    def check_jointstate(self, jointstate, delta_j):
+        if abs(jointstate[0]) > self.j1_limit:
+            jointstate[0] = jointstate[0] - delta_j[0]
+        if abs(jointstate[1]) > self.j6_limit:
+            jointstate[1] = jointstate[1] - delta_j[1]
+        if abs(jointstate[2]) > self.j1_limit:
+            jointstate[2] = jointstate[2] - delta_j[2]
+        if abs(jointstate[3]) > self.j4_limit:
+            jointstate[3] = jointstate[3] - delta_j[3]
+        if abs(jointstate[4]) > self.j1_limit:
+            jointstate[4] = jointstate[4] - delta_j[4]
+        if abs(jointstate[5]) > self.j6_limit:
+            jointstate[5] = jointstate[5] - delta_j[5]
+        if abs(jointstate[6]) > self.j7_limit:
+            jointstate[6] = jointstate[6] - delta_j[6]
+        return jointstate
+
     def applyAction(self, action):
         # dtargetVelocityL = action[0]
         # dtargetVelocityR = action[1]
         # targetvelocity=[dtargetVelocityL,dtargetVelocityR]
         # print('targetVelocity=',targetvelocity)
-        basevelocity = action[0]
-        baseangulervelocity = action[1]
-        djoint_1 = action[2]
-        djoint_2 = action[3]
-        djoint_3 = action[4]
-        djoint_4 = action[5]
-        djoint_5 = action[6]
-        djoint_6 = action[7]
-        djoint_7 = action[8]
+        # djoint_1 = action[2]
+        # djoint_2 = action[3]
+        # djoint_3 = action[4]
+        # djoint_4 = action[5]
+        # djoint_5 = action[6]
+        # djoint_6 = action[7]
 
-        self.basevelocity = self.basevelocity + basevelocity
-        self.baseangulervelocity = self.baseangulervelocity + baseangulervelocity
+        dbasevelocity = action[0]
+        dbaseangulervelocity = action[1]
+        djoint=np.array(action[2:9])
 
-        self.basevelocity = self.check_baseV(self.basevelocity, basevelocity)
-        self.baseangulervelocity = self.check_baseA(self.baseangulervelocity, baseangulervelocity)
+        if self.useSimulation:
+            baseve, baseva = p.getBaseVelocity(self.neobotixschunkUid)
+            self.basevelocity = baseve[0]
+            self.baseangulervelocity = baseva[2]
+            dae = []
+            for joint in self.joints:
+                self.Jointstate = p.getJointState(self.neobotixschunkUid, joint)
+                self.jointstate = self.Jointstate[0]
+                dae.append(self.jointstate)
+            self.jointposition = dae
+
+
+        self.basevelocity = self.basevelocity + dbasevelocity
+        self.baseangulervelocity = self.baseangulervelocity + dbaseangulervelocity
+
+        self.basevelocity = self.check_baseV(self.basevelocity, dbasevelocity)
+        self.baseangulervelocity = self.check_baseA(self.baseangulervelocity, dbaseangulervelocity)
 
         self.wheelVelR = ( self.basevelocity + 0.2535 * self.baseangulervelocity) / 0.13
         self.wheelVelL = ( self.basevelocity - 0.2535 * self.baseangulervelocity) / 0.13
 
-        self.wheelstate = [self.wheelVelL , self.wheelVelR]
+        self.wheelstate = [self.wheelVelL, self.wheelVelR]
         # dwe = []
         # for joint in self.wheels:
         #     self.Wheelstate = p.getJointState(self.neobotixschunkUid, joint)
@@ -131,21 +169,20 @@ class NeobotixSchunk:
         # dwl = dtargetVelocityL+dwe[0]
         # dwr = dtargetVelocityR+dwe[1]
         # self.wheelstate= [dwl,dwr]
-        dae = []
-        for joint in self.joints:
-            self.Jointstate=p.getJointState(self.neobotixschunkUid,joint)
-            self.jointstate=self.Jointstate[0]
-            dae.append(self.jointstate)
+
+
 
         # print('dae',dae)
-        da_1 = djoint_1+dae[0]
-        da_2 = djoint_2+dae[1]
-        da_3 = djoint_3+dae[2]
-        da_4 = djoint_4+dae[3]
-        da_5 = djoint_5+dae[4]
-        da_6 = djoint_6+dae[5]
-        da_7 = djoint_7+dae[6]
-        self.jointstate= [da_1,da_2,da_3,da_4,da_5,da_6,da_7]
+        # da_1 = djoint_1+dae[0]
+        # da_2 = djoint_2+dae[1]
+        # da_3 = djoint_3+dae[2]
+        # da_4 = djoint_4+dae[3]
+        # da_5 = djoint_5+dae[4]
+        # da_6 = djoint_6+dae[5]
+        # da_7 = djoint_7+dae[6]
+        # self.jointstate= [da_1,da_2,da_3,da_4,da_5,da_6,da_7]
+        self.jointposition= self.jointposition + djoint
+        self.jointposition = self.check_jointstate(self.jointposition, djoint)
 
         # print('targetAngle=', targetangle)
         for motor in self.wheels:
@@ -153,4 +190,53 @@ class NeobotixSchunk:
                                           targetVelocity=self.wheelstate[motor], force=self.maxForce)
         for motor_2 in self.joints:
             p.setJointMotorControl2(self.neobotixschunkUid, motor_2, p.POSITION_CONTROL,
-                                          targetPosition=self.jointstate[motor_2-6],force=self.maxForce)
+                                          targetPosition=self.jointposition[motor_2-6],force=self.maxForce)
+
+
+    def applyAction2(self, action):
+        dbasevelocity = action[0]
+        dbaseangulervelocity = action[1]
+        self.basevelocity = self.basevelocity + dbasevelocity
+        self.baseangulervelocity = self.baseangulervelocity + dbaseangulervelocity
+
+        self.basevelocity = self.check_baseV(self.basevelocity, dbasevelocity)
+        self.baseangulervelocity = self.check_baseA(self.baseangulervelocity, dbaseangulervelocity)
+
+        self.wheelVelR = (self.basevelocity + 0.2535 * self.baseangulervelocity) / 0.13
+        self.wheelVelL = (self.basevelocity - 0.2535 * self.baseangulervelocity) / 0.13
+
+        self.wheelstate = [self.wheelVelL, self.wheelVelR]
+
+        if (self.useInverseKinematics):
+            # change of endeffector position
+            dx = action[2]
+            dy = action[3]
+            dz = action[4]
+            self.endEffectorPos[0] = self.endEffectorPos[0] + dx
+            self.endEffectorPos[1] = self.endEffectorPos[1] + dy
+            self.endEffectorPos[2] = self.endEffectorPos[2] + dz
+            position = self.endEffectorPos
+            orientation = p.getQuaternionFromEuler([0, -math.pi, 0])
+            if (self.useNullSpace == 1):
+                if (self.useOrientation == 1):
+                    jointPoses = p.calculateInverseKinematics(self.neobotixschunkUid, self.neobotixschunkEndEffectorIndex, position, orientation,
+                                                              self.ll, self.ul, self.jr, self.rp)
+                else:
+                    jointPoses = p.calculateInverseKinematics(self.neobotixschunkUid, self.neobotixschunkEndEffectorIndex, position,
+                                                              lowerLimits=self.ll, upperLimits=self.ul,
+                                                              jointRanges=self.jr, restPoses=self.rp)
+            else:
+                if (self.useOrientation == 1):
+                    jointPoses = p.calculateInverseKinematics(self.neobotixschunkUid, self.neobotixschunkEndEffectorIndex, position, orientation,
+                                                              jointDamping=self.jd)
+                else:
+                    jointPoses = p.calculateInverseKinematics(self.neobotixschunkUid, self.neobotixschunkEndEffectorIndex, position)
+        print("jointPoses")
+        print(jointPoses)
+
+        for motor in self.wheels:
+            p.setJointMotorControl2(self.neobotixschunkUid, motor, p.VELOCITY_CONTROL,
+                                        targetVelocity=self.wheelstate[motor], force=self.maxForce)
+        for motor_2 in self.joints:
+            p.setJointMotorControl2(self.neobotixschunkUid, motor_2, p.POSITION_CONTROL,
+                                        targetPosition=jointPoses[motor_2-4 ], force=self.maxForce)
--- examples/pybullet/gym/pybullet_envs/bullet/neobotixschunkGymEnvTest.py
+++ examples/pybullet/gym/pybullet_envs/bullet/neobotixschunkGymEnvTest.py
@@ -8,18 +8,21 @@ def main():
 
     environment = neobotixschunkGymEnv.NeobotixSchunkGymEnv(renders=True,isDiscrete=False, maxSteps=100000000)
     dv = 0.05
-    dtheta = 1
+    dtheta = 0.1
     leftwheelVelocitySlider = environment._p.addUserDebugParameter("basevelocity",-dv,dv,0)
     rightwheelVelocitySlider = environment._p.addUserDebugParameter("baseangularvelocity",-dtheta,dtheta,0)
-    joint_1_Slider=environment._p.addUserDebugParameter("arm_1_joint",-dv,dv,0)
-    joint_2_Slider=environment._p.addUserDebugParameter("arm_2_joint",-dv,dv,0)
-    joint_3_Slider=environment._p.addUserDebugParameter("arm_3_joint",-dv,dv,0)
-    joint_4_Slider=environment._p.addUserDebugParameter("arm_4_joint",-dv,dv,0)
-    joint_5_Slider=environment._p.addUserDebugParameter("arm_5_joint",-dv,dv,0)
-    joint_6_Slider=environment._p.addUserDebugParameter("arm_6_joint",-dv,dv,0)
-    joint_7_Slider=environment._p.addUserDebugParameter("arm_7_joint",-dv,dv,0)
-    actionIds = [leftwheelVelocitySlider, rightwheelVelocitySlider, joint_1_Slider, joint_2_Slider, joint_3_Slider, joint_4_Slider, joint_5_Slider, joint_6_Slider, joint_7_Slider]
-
+    Endeffectorsliderx= environment._p.addUserDebugParameter("endeffector_x",-dtheta,dtheta,0)
+    Endeffectorslidery = environment._p.addUserDebugParameter("endeffector_y", -dtheta, dtheta, 0)
+    Endeffectorsliderz = environment._p.addUserDebugParameter("endeffector_z", -dtheta, dtheta, 0)
+    # joint_1_Slider=environment._p.addUserDebugParameter("arm_1_joint",-dv,dv,0)
+    # joint_2_Slider=environment._p.addUserDebugParameter("arm_2_joint",-dv,dv,0)
+    # joint_3_Slider=environment._p.addUserDebugParameter("arm_3_joint",-dv,dv,0)
+    # joint_4_Slider=environment._p.addUserDebugParameter("arm_4_joint",-dv,dv,0)
+    # joint_5_Slider=environment._p.addUserDebugParameter("arm_5_joint",-dv,dv,0)
+    # joint_6_Slider=environment._p.addUserDebugParameter("arm_6_joint",-dv,dv,0)
+    # joint_7_Slider=environment._p.addUserDebugParameter("arm_7_joint",-dv,dv,0)
+    #  actionIds = [leftwheelVelocitySlider, rightwheelVelocitySlider, joint_1_Slider, joint_2_Slider, joint_3_Slider, joint_4_Slider, joint_5_Slider, joint_6_Slider, joint_7_Slider]
+    actionIds = [leftwheelVelocitySlider, rightwheelVelocitySlider,Endeffectorsliderx,Endeffectorslidery,Endeffectorsliderz]
     done = 0
     while(not done):
         # leftwheelVelocity = environment._p.readUserDebugParameter(leftwheelVelocitySlider)
